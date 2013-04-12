02a8ba7dede953836d1358bd7a532fe117f805ab
Ngurah Mantara <ngurahmantara@gmail.com>
2013-04-12 10:51:09 +0800

    fix footer note

--- application/views/template/index.php
+++ application/views/template/index.php
@@ -111,7 +111,7 @@
     
     <!-- Footer line -->
     <div id="footer">
-        <div class="wrapper">As usually all rights reserved. And as usually brought to you by <a href="http://themeforest.net/user/Kopyov?ref=kopyov" title="">Eugene Kopyov</a></div>
+        <div class="wrapper">copyright @ dps.gapura.co.id</div>
     </div>
 
 </div>
