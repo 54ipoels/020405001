94dadcc69e6137ac9a6ace174d932e2bbad9df61
Ngurah Mantara <ngurahmantara@gmail.com>
2013-06-21 16:54:40 +0800

    fix api controller

--- application/controllers/api/pekerja_data_pribadi.php
+++ application/controllers/api/pekerja_data_pribadi.php
@@ -67,5 +67,35 @@ class Pekerja_data_pribadi extends REST_Controller {
 		}
 	}
 	
+	public function orangtua_get()
+	{
+		if(!$this->get('nipp'))
+        {
+        	$this->response(NULL, 400);
+        }
+		else
+		{
+			$nipp = $this->get('nipp');
+			$data = $this->detail_kepegawaian->get_detail_pegawai_ayah($nipp);
+			$data = $this->detail_kepegawaian->get_detail_pegawai_ibu($nipp);
+			$this->response($data, 200); // 200 being the HTTP response code
+		}
+	}
+	
+	public function mertua_get()
+	{
+		if(!$this->get('nipp'))
+        {
+        	$this->response(NULL, 400);
+        }
+		else
+		{
+			$nipp = $this->get('nipp');
+			$data = $this->detail_kepegawaian->get_detail_pegawai_mert_ayah($nipp);
+			$data = $this->detail_kepegawaian->get_detail_pegawai_mert_ibu($nipp);
+			$this->response($data, 200); // 200 being the HTTP response code
+		}
+	}
+	
 }
 ?>
